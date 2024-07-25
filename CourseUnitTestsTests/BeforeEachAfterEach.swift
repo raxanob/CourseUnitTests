a823b7a5fc1c03b75da5c842e38a8b36f8175d7f
raxanob <rayanexavier161@gmail.com>
2024-07-25 20:21:51 -0300

    introducao ao quick and nimble e beforeeach and aftereach

---
+++ CourseUnitTestsTests/BeforeEachAfterEach.swift
@@ -0,0 +1,12 @@
+//
+//  BeforeEachAfterEach.swift
+//  CourseUnitTestsTests
+//
+//  Created by Rayane Xavier on 25/07/24.
+//
+
+import UIKit
+
+class BeforeEachAfterEach: QuickSpec {
+
+}
---
+++ CourseUnitTestsTests/StringUtilsTests.swift
@@ -0,0 +1,12 @@
+//
+//  StringUtilsTests.swift
+//  CourseUnitTestsTests
+//
+//  Created by Rayane Xavier on 25/07/24.
+//
+
+import UIKit
+
+class StringUtilsTests: NSObject {
+
+}
