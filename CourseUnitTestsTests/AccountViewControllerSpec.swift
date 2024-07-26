8bbd684eb07e3209513aee8133a0cdc374db16e6
raxanob <rayanexavier161@gmail.com>
2024-07-25 22:24:21 -0300

    testes view controller e mocks

---
+++ CourseUnitTestsTests/AccountViewControllerSpec.swift
@@ -0,0 +1,12 @@
+//
+//  AccountViewControllerSpec.swift
+//  CourseUnitTestsTests
+//
+//  Created by Rayane Xavier on 25/07/24.
+//
+
+import UIKit
+
+class AccountViewControllerSpec: QuickSpec {
+
+}
